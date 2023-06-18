32e001554328bd3250c041e5992d15c2eba7c9e0
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2023-06-18 19:00:19 +0300

    ending the header

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,13 +1,14 @@
 import React,{Fragment} from 'react';
 import styles from './Header.module.css'
 import mealsImage from '../../assets/meals.jpg';
+import HeaderCarttButton from './HeaderCarttButtton';
 
 const Header = props => {
     return (
         <Fragment >
             <header className={styles.header}>
                 <h1>ReactMeals</h1>
-                <button>Cart</button>
+                <HeaderCarttButton>Cart</HeaderCarttButton>
             </header>
 
             <div className={styles['main-image']}>
---
+++ src/components/Layout/HeaderCarttButtton.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styels from './HeaderCarttButton.module.css';  
+import CartIcon from "../Cart/CartIcon";
+const HeaderCarttButton = props => {
+    return (
+        <button className={ styels.button}>
+            <span  className={styels.icon} ><CartIcon/></span>
+            
+            <span>{props.children}</span>
+
+            <span className={styels.badge}>3</span>
+        </button>
+    )
+}
+
+export default HeaderCarttButton;
