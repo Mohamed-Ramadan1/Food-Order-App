83ebffbeeaadccc7f45c6ef95cfe37ff3f18713e
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2023-06-18 20:06:16 +0300

    creating item and styles for it

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,5 +1,8 @@
 import React from "react";
+
 import styles from './AvilableMeals.module.css'
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
 
 
 const DUMMY_MEALS = [
@@ -29,12 +32,19 @@ const DUMMY_MEALS = [
     },
 ];
 const AvilableMeals = () => {
-    const mealsList = DUMMY_MEALS.map(meal => <li key={meal.id}>{meal.name }</li> )
+    const mealsList = DUMMY_MEALS.map(meal => <MealItem
+        key={meal.id}
+        name={meal.name}
+        description={meal.description}
+        price={meal.price}
+    />)
     return (
         <section className={styles.meals}>
-            <ul>
-                { mealsList}
-            </ul>
+            <Card>
+                <ul>
+                    { mealsList}
+                </ul>
+            </Card>
         </section>
     )
 }
