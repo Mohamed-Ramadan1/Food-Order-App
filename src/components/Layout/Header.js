47cf4d0ed177bbfdaedd1547639cf12b2f4b2e4e
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2023-06-18 18:33:48 +0300

    render header to app componemt

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import styles from './Header'
+import React,{Fragment} from 'react';
+import styles from './Header.module.css'
+import mealsImage from '../../assets/meals.jpg';
 
 const Header = props => {
     return (
-        <>
-        </>
-    )
-}
+        <Fragment >
+            <header className={styles.header}>
+                <h1>ReactMeals</h1>
+                <button>Cart</button>
+            </header>
+
+            <div className={styles['main-image']}>
+                <img
+                    src={mealsImage}
+                    alt='Meal Image'
+                    className={styles['main-image']}
+                />
+            </div>
+    
+        </Fragment>
+    );
+};
 
 
 export default Header;
