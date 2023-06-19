c3e3b8cdb2a1dead9a5ee68178e7970a29935b96
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2023-06-19 09:18:23 +0300

    creating UI component

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,8 +1,15 @@
 import React from "react";
+import styles from './MealItemForm.module.css'
+import Input from "../../UI/Input";
 
-
-const MealItemForm = () => {
-    return;
+const MealItemForm = props => {
+    return (
+        <form className={styles.form} >
+            <Input/>
+        
+            <button>+ Add</button>
+        </form>
+    )
 }
 
 export default MealItemForm;
