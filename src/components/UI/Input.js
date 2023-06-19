bdfdf162be2354aa03d44dd8c35cc62b0b2a67cd
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2023-06-19 09:38:35 +0300

    end form

--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,7 +1,13 @@
 import React from "react";
+import styles from './Input.module.css'
+const Input = props => {
+    return (
+        <div className={styles.input}>
+            <label htmlFor={props.input.id}>{props.label}</label>
+            <input  {...props.input}/>
 
-const Input = () => {
-    return <input type="text"/>
+        </div>
+    )
 }
 
 export default Input;
