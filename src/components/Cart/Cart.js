1f93434c00f4c0089d50085c3d2e8a82f5faa154
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2023-06-20 10:29:00 +0300

    creating cart  scroll and handler the cart look

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,6 +2,7 @@ import React,{useContext} from "react";
 import styles from './Cart.module.css';
 import Modal from "../UI/Modal";
 import CartContext from "../../store/cart-context";
+import CartItem from "./CartItem";
 
 
 const Cart = props => {
@@ -10,10 +11,20 @@ const Cart = props => {
     const totalAmount = `$${cartCtx.totoalAmount.toFixed(2)}`;
     const hasItems = cartCtx.items.length > 0;
 
+    const cartItemRemoveHandler=id=>{}
+    const cartItemAddHandler=item=>{}
+
     const cartItems =
         <ul className={styles['cart-items']}>
             {
-                cartCtx.items.map(item => <li>{item.name}</li>)
+                cartCtx.items.map(item => <CartItem
+                    key={item.id}
+                    name={item.name}
+                    amount={item.amount}
+                    price={item.price}
+                    onRemove={cartItemRemoveHandler.bind(null,item.id)}
+                    onAdd={cartItemAddHandler.bind(null,item)}
+                />)
             }
         </ul>
 
