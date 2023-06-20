1178dad953ce68cf1519d9fd72e62ef242f584ee
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2023-06-20 11:13:44 +0300

    ending cart operations and logic

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -7,22 +7,52 @@ const defaultCartState = {
 }
 const cartReducer = (state, action) => {
     if (action.type === 'ADD') { 
-        const updatedItems = state.items.concat(action.item);
-        const updatedTotalAmount = state.totoalAmount + ( action.item.price * action.item.amount);
+        const updatedTotalAmount = state.totoalAmount + (action.item.price * action.item.amount);
         
+        const existingCartItemIndex = state.items.findIndex(
+            item => item.id === action.item.id
+        );
+        const existCartItem = state.items[existingCartItemIndex];
+        let updatedItems;
+        
+        if (existCartItem) {
+            
+            const updatedItem = {
+                ...existCartItem,
+                amount: existCartItem.amount + action.item.amount
+            };
+            updatedItems = [...state.items]; 
+            updatedItems[existingCartItemIndex] = updatedItem;
+        } else {
+            updatedItems = state.items.concat(action.item);
+        }
     return {
         items: updatedItems,
         totoalAmount:updatedTotalAmount
     };
 
     }
-    
-    if (action.type === 'REMOVE') {
 
+    if (action.type === 'REMOVE') {
+        const existingCartItemIndex = state.items.findIndex(
+            item => item.id === action.id
+        );
+        const existingItem = state.items[existingCartItemIndex];
+        let updatedTotalAmount = state.totoalAmount - existingItem.price;
+        let updatedItems;
+        if (existingItem.amount === 1) {
+            updatedItems = state.items.filter(item => item.id !== action.id);
+        } else {
+            const updatedItem = { ...existingItem, amount: existingItem.amount - 1 }
+            updatedItems = [...state.items];
+            updatedItems[existingCartItemIndex] = updatedItem;
+        }
+        return {
+            items: updatedItems,
+            totoalAmount:updatedTotalAmount
+        }
     }
     
-
-
     return defaultCartState
 }
 
