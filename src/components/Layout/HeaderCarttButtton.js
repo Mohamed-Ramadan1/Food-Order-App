dc859082bc13666aabe3b86fef663a9af85b59a0
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2023-06-20 11:30:01 +0300

    Ending the first part of the project .

--- src/components/Layout/HeaderCarttButtton.js
+++ src/components/Layout/HeaderCarttButtton.js
@@ -1,14 +1,30 @@
-import React,{useContext} from "react";
+import React,{useContext,useEffect,useState} from "react";
 import styels from './HeaderCarttButton.module.css';  
 import CartContext from "../../store/cart-context";
 import CartIcon from "../Cart/CartIcon";
 const HeaderCarttButton = props => {
+    const[btnIshHilighted,setBtnIshHilighted]=useState(false)
     const cartCtx = useContext(CartContext);
     const cartItemsNumber = cartCtx.items.reduce((current, item) => {
-        return current+item.amount
-    },0)
+        return current + item.amount
+    }, 0);
+
+    const { items } = cartCtx;
+    const btnClass = `${styels.button} ${btnIshHilighted?styels.bump:""}`;
+
+    useEffect(() => {
+        if (items.length === 0) return;
+
+        setBtnIshHilighted(true);
+        const timer=setTimeout(() => { setBtnIshHilighted(false) }, 300);
+        
+        return () => {
+            clearTimeout(timer);
+        };
+
+    }, [items]);
     return (
-        <button className={ styels.button} onClick={props.onClick}>
+        <button className={ btnClass} onClick={props.onClick}>
             <span  className={styels.icon} ><CartIcon/></span>
             
             <span>{props.children}</span>
