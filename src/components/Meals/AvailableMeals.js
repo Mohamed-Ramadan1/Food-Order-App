3ed2bd4560d18ae5e0e6cd1a48593d69e16019b3
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2023-06-29 23:43:46 +0300

    updating the featching data from the server

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,51 +1,25 @@
-<<<<<<< HEAD
 import React,{useEffect,useState} from "react";
 
-=======
-import React from "react";
->>>>>>> 261dc7cfd0c1ced0e02e0909ff25e07d5a0096e1
 import styles from './AvilableMeals.module.css'
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
 
 
 
-<<<<<<< HEAD
-=======
-const DUMMY_MEALS = [
-    {
-        id: 'm1',
-        name: 'Sushi',
-        description: 'Finest fish and veggies',
-        price: 22.99,
-    },
-    {
-        id: 'm2',
-        name: 'Schnitzel',
-        description: 'A german specialty!',
-        price: 16.5,
-    },
-    {
-        id: 'm3',
-        name: 'Barbecue Burger',
-        description: 'American, raw, meaty',
-        price: 12.99,
-    },
-    {
-        id: 'm4',
-        name: 'Green Bowl',
-        description: 'Healthy...and green...',
-        price: 18.99,
-    },
-];
->>>>>>> 261dc7cfd0c1ced0e02e0909ff25e07d5a0096e1
 const AvilableMeals = () => {
-    const [meals,setMeals]=useState([])
+    const [meals, setMeals] = useState([]);
+    const [isLoading,setIsLoading] = useState(true);
+    const [hasError, setHasError] = useState();
+    
     useEffect(() => {
-
         const fetchMeals = async () => {
-            const response = await fetch("https://food-order-app-e13f9-default-rtdb.firebaseio.com/meals.json");
-            const responseData = await response.json();
+            const response = await fetch("https://Food-order-app-e13f9-default-rtdb.firebaseio.com/meals.json");
+
+            if (!response.ok) {
+                throw new Error('somthing went wront!');
+            }
+
+            const responseData = await response.json(); 
             const loadedMeals = [];
 
             for (const key in responseData) {
@@ -57,9 +31,24 @@ const AvilableMeals = () => {
                 })
             }
             setMeals(loadedMeals);
+            setIsLoading(false);
+
         };
-        fetchMeals();
+
+        
+        fetchMeals().catch((error) => {
+            setIsLoading(false);
+            setHasError(error.message)
+        });
+        
     }, []);
+
+    if (isLoading) {
+        return <section className={styles.MelasLoading}><p>Loading...</p></section>
+    }
+    if (hasError ) {
+        return <section className={styles.MealsError}><p>{hasError}</p></section>
+    }
 
     const mealsList = meals.map(meal => <MealItem
         id={meal.id}
