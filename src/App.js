c237720815c35c14fcb648ca3637bf7dae876021
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2023-08-03 08:46:23 +0300

    Recreate the project and fixed some issuse.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,5 @@
-import React,{useState} from "react";
-import Header from "./components/Layout/Header";
-import Meals from "./components/Meals/Meals";
-import Cart from "./components/Cart/Cart";
-import CartPorvider from "./store/CartProvider";
-
-
 function App() {
-  const [cartIsShaown, setCartIsShaown] = useState(false);
-
-  const showCartHandler = () => {
-      setCartIsShaown(true)
-  }
-  
-  const hideCartHandler = () => {
-      setCartIsShaown(false)
-}
-  return (
-    <CartPorvider>
-      {cartIsShaown&& <Cart onHideCart={hideCartHandler} />}
-      <Header onShowCard={showCartHandler} />
-      <main>
-      <Meals/>
-      </main>
-    </CartPorvider>
-  );
+  return <></>;
 }
 
 export default App;
