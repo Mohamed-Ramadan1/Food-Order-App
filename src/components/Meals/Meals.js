697b93be6d9dd2d410ee7eeaf72cd2bc528bdd1b
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2023-08-03 10:50:38 +0300

    Ending the layout and ready to start the logic.

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,14 @@
+import React from "react";
+import MealsSummary from "./MealsSummary";
+import AvilableMeals from "./AvilableMeals";
+
+const Meals = () => {
+  return (
+    <>
+      <MealsSummary />
+      <AvilableMeals />
+    </>
+  );
+};
+
+export default Meals;
